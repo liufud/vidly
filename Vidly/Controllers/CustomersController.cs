1db43c5945167309f3f2a571f4e1137c0a2fcd54
Jacky <liufudijacky@hotmail.com>
2019-01-27 20:40:00 -0500

    Added Movie and Customer

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customer> _customers = new List<Customer>
+        {
+            new Customer {Name = "John Smith", Id=0},
+            new Customer {Name = "Bob Hopkins", Id=1}
+        };
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var viewModel = new RandomMovieViewModel {Customers = _customers};
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(_customers.Find(x => x.Id == id));
+        }
+    }
+}
